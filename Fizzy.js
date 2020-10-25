fdb731ab697e35787e2f2209cfd77ed7e53b804a
Ilyas Jibril <jibriily2@gmail.com>
2020-10-25 16:20:06 -0400

    Initial commit

---
+++ Fizzy.js
@@ -0,0 +1,16 @@
+function fizzy(num) {
+  if (num % 3 === 0 && num % 5 !== 0) {
+    return "Fizz";
+  } else if (num % 5 === 0 && num % 3 !== 0) {
+    return "Buzz";
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else {
+    // (num % 3 !== 0 && num % 5 !== 0)
+    return num;
+  }
+}
+
+module.exports = {
+  fizzy,
+};
